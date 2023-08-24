8fefb1f95beba0e9adca8c8d4562b54196f2ff28
Dawn Trail <dtrail76@gmail.com>
2023-08-24 22:55:56 +0100

    update cs pages with dummy text

--- app/customer-services/size-guide/page.tsx
+++ app/customer-services/size-guide/page.tsx
@@ -1,10 +1,63 @@
 "use client";
-import Link from "next/link";
+import Header from "@/Components/Header/Header";
 
 function Page() {
   return (
     <main className=''>
-      <h1>Contact Us</h1>
+      <Header title='Size Guide' />
+      <div>
+        Ea eiusmod nostrud laboris ea ipsum sint consectetur elit id. Sint
+        veniam proident reprehenderit aliquip mollit dolor consectetur ad sit
+        amet mollit. Sint adipisicing qui tempor dolor. Esse id commodo
+        excepteur aliqua nostrud do. Occaecat consequat irure Lorem tempor
+        mollit in sint. Nostrud tempor qui voluptate officia cillum cillum id
+        cupidatat consequat eiusmod consequat fugiat qui. Ut amet proident
+        cupidatat deserunt magna commodo voluptate officia id cillum officia
+        laboris cupidatat. Dolore in amet pariatur eiusmod pariatur eiusmod
+        laborum enim consectetur dolor fugiat pariatur. Consequat ex cillum
+        dolor aute ut nostrud proident laboris amet sit. Veniam aute aute dolor
+        ad veniam laboris commodo nostrud cillum non id. Enim do labore minim
+        aliquip consectetur ex sit ut Lorem. Eiusmod ipsum exercitation esse
+        tempor eu aliqua fugiat fugiat cupidatat cillum eiusmod aliqua
+        cupidatat. Aliqua deserunt sint et sint occaecat enim ea ullamco.
+        Deserunt eiusmod nostrud do labore incididunt irure labore anim minim
+        minim veniam anim. Reprehenderit aute anim reprehenderit in consectetur
+        occaecat officia pariatur nulla laboris eiusmod. Nulla do ut nostrud
+        elit minim deserunt do incididunt. Aute ex duis quis laboris elit nisi
+        non. Consequat sit id sit eiusmod nisi dolor tempor velit Lorem do enim.
+        Mollit ad reprehenderit pariatur velit consequat dolore laboris veniam
+        cillum non. Officia minim qui nulla irure sit aliqua sit laborum minim
+        nulla. Esse do sint consectetur esse sit adipisicing consequat culpa
+        excepteur ex eu consequat. Amet consectetur pariatur pariatur qui minim
+        irure. Consectetur elit est exercitation excepteur excepteur.
+        Reprehenderit elit qui elit labore minim ex nulla. Consequat consequat
+        anim est minim duis non qui amet officia officia consectetur cillum
+        consequat. Esse esse officia commodo do non duis ut consequat. Ut ad
+        excepteur sit reprehenderit occaecat fugiat laborum enim nulla. Laboris
+        minim magna quis occaecat aliqua reprehenderit dolor proident
+        consectetur tempor do. Ex culpa mollit eiusmod labore excepteur elit
+        pariatur excepteur Lorem esse est aliquip. Exercitation duis qui
+        incididunt amet sunt exercitation sit ad qui amet officia laboris
+        deserunt irure. Laboris adipisicing incididunt nostrud eu sint ea nulla
+        tempor cillum consectetur exercitation laboris ex eu. Consequat commodo
+        ullamco ad quis in id ea fugiat eiusmod eu dolore culpa. Sint incididunt
+        id et duis. Mollit amet Lorem do quis Lorem magna nulla duis pariatur
+        magna aliqua officia. Fugiat irure cupidatat do anim ut sunt in sit
+        magna minim in quis aliquip. Velit ex ea aliqua duis nisi sint culpa
+        fugiat reprehenderit. Culpa et dolor enim mollit veniam fugiat ut sit
+        officia eu elit aute sint consectetur. Mollit officia non dolore nisi
+        anim velit est culpa sint. Proident deserunt sint nostrud nostrud culpa
+        in qui proident exercitation cupidatat. Reprehenderit minim quis
+        reprehenderit aute anim ad labore. Voluptate in ea voluptate officia
+        enim cupidatat ut veniam dolore esse incididunt culpa. Sit amet
+        adipisicing velit elit mollit. Aliquip enim anim laborum anim ullamco
+        nulla in voluptate duis adipisicing anim. Fugiat officia id aliquip
+        laborum proident occaecat anim commodo fugiat. Nostrud laborum sit
+        adipisicing aliquip elit sunt mollit cupidatat voluptate est. Aliqua
+        fugiat voluptate officia exercitation laborum tempor adipisicing commodo
+        quis. Eu proident duis incididunt culpa adipisicing esse consequat
+        proident aute occaecat..
+      </div>
     </main>
   );
 }
