ceb471e14a9923ec128364a31dfecceb984228ed
Dawn Trail <dtrail76@gmail.com>
2023-08-24 22:56:56 +0100

    update layout for mobile and desktop

--- app/customer-services/layout.tsx
+++ app/customer-services/layout.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import Nav from "../../Components/Nav/Nav";
 import { navLinks } from "../../Components/Nav/navLinks";
 import HomeIcon from "@mui/icons-material/Home";
-import Header from "@/Components/Header/Header";
 
 export default function CustomerSeviceLayout({
   children,
@@ -11,14 +10,14 @@ export default function CustomerSeviceLayout({
 }) {
   return (
     <section className='md:flex justify-between md:h-screen mt-2'>
-      <div className='w-1/4'>
-        <Link href='/' className='p-4'>
+      <div className='w-full md:w-1/4 flex justify-center bg-white'>
+        <Link href='/' className='absolute left-4 top-2 z-50'>
           <HomeIcon color={"primary"} />
         </Link>
 
         <Nav navLinks={navLinks} />
       </div>
-      <div className='w-3/4 border-l-4 pl-8'>{children}</div>
+      <div className='w-full md:w-3/4 border-l-4 pl-8'>{children}</div>
     </section>
   );
 }
